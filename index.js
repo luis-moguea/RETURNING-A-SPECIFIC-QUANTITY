0d95e238e96dbec0ba29bc3a617ad78bb3a683cb
Luis <luis.mogueadp@gmail.com>
2022-04-24 11:31:16 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+function doubleOut(quantity){
+    
+    if (quantity > 1) {
+        return `There are ${quantity} cars`       
+    } 
+    else{
+        return `There is just one car`}
+}
+
+console.log(doubleOut(1))
